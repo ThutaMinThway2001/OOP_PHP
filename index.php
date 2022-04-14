6f1d3238530cbe84d4bf98d168999cc2b2d9c2cb
Thuta Min Thway <thutaminthway2001@gmail.com>
2022-04-14 21:25:07 +0630

    Inheritance

--- index.php
+++ index.php
@@ -1,61 +1,17 @@
 <?php
-
-class Team{
-    protected $name;
-    protected $members = [];
-    protected $age;
-
-    public function __construct($name, $members = [], $age)
-    {
-        $this->name = $name;
-        $this->members = $members;
-        $this->age = $age;
+    //parent
+    class Team{
+        public function money(){
+            var_dump("Team's Money");
+        }
+    }
+    //child
+    class Department extends Team{
+        public function useMoney(){
+            var_dump("Use Money");
+        }
     }
 
-    public function name(){
-        return $this->name;
-    }
+    (new Department())->money();
 
-    public static function start(...$params){
-        return new static(...$params);
-    }
-
-    public function members(){
-        return $this->members;
-    }
-
-    public function add($name){
-        $this->members[] = $name;
-    }
-
-    public function cancel(){
-
-    }
-
-    public function admin(){
-
-    }
-}
-
-class Member{
-    protected $name;
-
-    public function __construct($name)
-    {
-        $this->name = $name;
-    }
-}
-
-$object1 = Team::start('Laravel', [
-    new Member('Kyaw Kyaw'),
-    new Member('Aung Aung')
-], 20);
-
-$object2 = Team::start('Css', [
-    new Member('Kyaw Kyaw'),
-    new Member('Aung Aung')
-], 20);
-
-var_dump($object1);
-var_dump($object2);
 ?>
